11a22c80cae82ef3d980df4087ca8973de57a72b
JamieDanielson <jamieedanielson@gmail.com>
2019-04-06 15:49:49 -0400

    Update to catch exceptions
    A few things were wrong with the first attempt. First, there was no Try Except block.  The float would immediately cause an error if the input was non-numeric.  Now, if a non-numeric input is entered, the result will be 'Bad score'.  Second, the line for an F score was redundant in having <1.0 and <0.6.  The <1.0 is no longer necessary since a number less than 0.6 is already less than 1.0.  Third, it's necessary to ensure the entry is greater than 0 to remain in range, because otherwise a negative input would count as an F.

--- chapter-3/3.3_score.py
+++ chapter-3/3.3_score.py
@@ -7,18 +7,24 @@
 # < 0.6 F
 # If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
 
-score = float(input('Enter Score:'))
+inp = input('Enter score: ')
 
-if score < 1.0 and score >= 0.9:
-    print('A')
-elif score < 1.0 and score >= 0.8:
-    print('B')
-elif score < 1.0 and score >= 0.7:
-    print('C')
-elif score < 1.0 and score >= 0.6:
-    print('D')
-elif score < 1.0 and score < 0.6:
-    print('F')
-else :
-    print('Invalid Entry. Score must be between 0 and 1')
-    quit()
+try:
+  score = float(inp)
+  if score >= 0.9 and score <= 1.0:
+    print ('A')
+  elif score >= 0.8 and score <= 1.0:
+    print ('B')
+  elif score >= 0.7 and score <= 1.0:
+    print ('C')
+  elif score >= 0.6 and score <= 1.0:
+    print ('D')
+  elif score < 0.6 and score >= 0:
+    print ('F')
+  else:
+    print ('Bad score')
+except:
+  print ('Bad score')
+
+  quit()
+    
